fabc3e299d211f848cdc9e2547f16c7d4feb7a60
uemirhanselim <uemirhanselim@gmail.com>
2022-11-17 20:53:26 +0300

    search view created

---
+++ lib/view/search_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SearchView extends StatefulWidget {
+const SearchView({Key? key}) : super(key: key);
+@override
+State<SearchView> createState() => SearchViewState();
+}
+class SearchViewState extends State<SearchView> {
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(),
+);
+}
+}
