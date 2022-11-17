51e81cbd6137e72ae5964b092acc652a6fd835da
uemirhanselim <uemirhanselim@gmail.com>
2022-11-17 20:53:35 +0300

    shopping view created

---
+++ lib/view/shopping_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ShoppingView extends StatefulWidget {
+const ShoppingView({Key? key}) : super(key: key);
+@override
+State<ShoppingView> createState() => ShoppingViewState();
+}
+class ShoppingViewState extends State<ShoppingView> {
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(),
+);
+}
+}
