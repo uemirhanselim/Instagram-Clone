f36132905ba6dff34d0ec6e5edae3dbead83c852
uemirhanselim <uemirhanselim@gmail.com>
2022-11-17 20:53:19 +0300

    reels view created

---
+++ lib/view/reels_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ReelsView extends StatefulWidget {
+const ReelsView({Key? key}) : super(key: key);
+@override
+State<ReelsView> createState() => ReelsViewState();
+}
+class ReelsViewState extends State<ReelsView> {
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(),
+);
+}
+}
